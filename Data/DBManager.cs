37eeca9b5b21b3392955f646f06d886aff7bfb0a
Yoseph <yoseph.campbellhw@gmail.com>
2025-10-18 11:43:04 +0800

    Added: Web Data Service Layer

--- Data/DBManager.cs
+++ Data/DBManager.cs
@@ -1,6 +1,21 @@
-﻿namespace WebService.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WebService.Models;
+
+namespace WebService.Data
 {
-    public class DBManager
+    public class DBManager : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(@"Data Source= PeerToPeer.db; foreign keys = true;");
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            List<Client> clients = new List<Client>();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using WebService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DBManager>();
 
 var app = builder.Build();
 
