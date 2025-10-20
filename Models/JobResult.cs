861317bc861c554ea80eb24eaa803a058c988859
Yoseph <yoseph.campbellhw@gmail.com>
2025-10-20 15:08:21 +0800

    Added: JobResult model to the data layer

---
+++ Models/JobResult.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebService.Models
+{
+    public class JobResult
+    {
+        public int? Id { get; set; } //pk
+        public int ClientId { get; set; }
+        public string Result { get; set; }
+    }
+}
